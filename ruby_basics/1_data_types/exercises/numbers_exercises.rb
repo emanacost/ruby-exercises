32c0ceaf1de7b64259c10e5419eb9154c8ed12fa
eacosta <eacosta0411@gmail.com>
2025-09-01 19:11:25 -0400

    Half of numbers tests done

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -5,22 +5,27 @@ end
 
 def subtract(a, b)
   # return the result of subtracting b from a
+  a - b
 end
 
 def multiply(a, b)
   # return the result of multiplying a times b
+  a * b
 end
 
 def divide(a, b)
   # return the result of dividing a by b
+  a / b
 end
 
 def remainder(a, b)
   # return the remainder of dividing a by b using the modulo operator
+  a % b
 end
 
 def float_division(a, b)
   # return the result of dividing a by b as a float, rather than an integer
+  a / b.to_f
 end
 
 def string_to_number(string)
